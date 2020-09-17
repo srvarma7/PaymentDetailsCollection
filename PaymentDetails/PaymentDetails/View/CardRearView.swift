bdf2b3f1f467ebe16aea0a98abcd109ae31f0472
srvarma7 <srvarma7@gmail.com>
2020-09-17 16:22:15 +1000

    Implementing card rear view

--- PaymentDetails/PaymentDetails/View/CardRearView.swift
+++ PaymentDetails/PaymentDetails/View/CardRearView.swift
@@ -8,13 +8,76 @@
 import SwiftUI
 
 struct CardRearView: View {
+    
+    var name: String
+    var cvv: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading) {
+            
+            HStack {
+                
+            }
+            .frame(width: 400, height: 35)
+            .background(Color.black)
+            .padding([.top, .bottom], 15)
+            
+            
+            HStack {
+                
+                VStack(alignment: .leading) {
+                    
+                    Text("Signature")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.gray)
+                        .padding([.leading], 10)
+                        .padding([.bottom], 0.2)
+                    
+                    Text(name == "" ? "Enter name" : name)
+                        .font(.custom("Noteworthy-Light", size: 20))
+                        .fontWeight(.bold)
+                        .frame(width: 200, height: 40, alignment: .leading)
+                        .padding(.leading, 5)
+                        .background(Color.gray)
+                        .foregroundColor(.white)
+                        .padding(.leading, 10)
+                    
+                }
+                
+                VStack(alignment: .trailing) {
+                    
+                    Text("CVV")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.gray)
+                        .padding([.bottom], 0.2)
+                    
+                    Text(cvv == "" ? "---" : cvv)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .padding(.trailing, 0.2)
+                        .frame(width: 50, height: 40, alignment: .leading)
+                        .background(Color.gray)
+                        .foregroundColor(.white)
+                    
+                }.offset(x: -10)
+                
+                
+            }.padding()
+            
+            Spacer()
+        }
+        .padding()
+        .frame(width: 400, height: 260, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing))
+    
     }
 }
 
 struct CardRearView_Previews: PreviewProvider {
     static var previews: some View {
-        CardRearView()
+        CardRearView(name: "", cvv: "")
     }
 }
